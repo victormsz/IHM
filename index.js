aea269bd2b60985fdc46c364d08ba4de174f30ea
Victor Z <victormsz09@yahoo.com.br>
2025-04-01 16:15:58 -0300

    readded

---
+++ index.js
@@ -0,0 +1,37 @@
+function openMenu() {
+    document.getElementById("menu_aba").style.width = "100%"; 
+  }
+  
+  function closeMenu() {
+    document.getElementById("menu_aba").style.width = "0";    
+  }
+  
+  function temaLim() {
+      document.documentElement.style.setProperty('--cor-click', '#38184C');
+      document.documentElement.style.setProperty('--cor-sombra', '#9b0a59');
+      document.documentElement.style.setProperty('--cor-text', 'black');
+      document.documentElement.style.setProperty('--cor-back1', '#CEF09D');
+      document.documentElement.style.setProperty('--cor-back2', '#4f6a93');
+      document.documentElement.style.setProperty('--md-sys-color-primary', '#38184C');
+  }
+  
+  function temaDark() {
+      document.documentElement.style.setProperty('--cor-click', '#CEF09D');
+      document.documentElement.style.setProperty('--cor-sombra', '#9b0a59');
+      document.documentElement.style.setProperty('--cor-text', 'black');
+      document.documentElement.style.setProperty('--cor-back1', '#38184C');
+      document.documentElement.style.setProperty('--cor-back2', '#4f6a93');
+      document.documentElement.style.setProperty('--md-sys-color-primary', '#CEF09D');
+  }
+  
+  function temaInatel() {
+      document.documentElement.style.setProperty('--cor-click', '#126ae2');
+      document.documentElement.style.setProperty('--cor-sombra', '#0a599b');
+      document.documentElement.style.setProperty('--cor-text', 'black');
+      document.documentElement.style.setProperty('--cor-back1', '#edf2f4');
+      document.documentElement.style.setProperty('--cor-back2', '#6a937a');
+      document.documentElement.style.setProperty('--md-sys-color-primary', '#126ae2');
+    
+  }
+  
+  
